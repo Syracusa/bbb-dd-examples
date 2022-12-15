194dc941cf13f6ef23446f2117de34b07ed6432b
Syracusa <msrndsy@gmail.com>
2022-12-15 18:21:51 +0900

    pinmux update

--- pinmux_ex/pinmux_ex.c
+++ pinmux_ex/pinmux_ex.c
@@ -1,13 +1,31 @@
 #include <linux/init.h>
 #include <linux/module.h>
 
+#include "bbb_pinmux.h"
+
 MODULE_LICENSE("GPL");
 
-#define BBB_CTRL_BASE   0x0
+static void set_pinmux(volatile unsigned int addr, int mmode)
+{
+    struct am335x_conf_regval val;
+    val.slewctrl = 0;
+    val.rxactive = 1;
+    val.pulltypesel = 1;
+    val.puden = 0;
+    val.mmode = mmode;
+
+    writel(addr, val);
+}
 
 static int pinmux_ex_init(void)
 {
     printk(KERN_NOTICE "BBB PINMUX Test driver init\n");
+
+    /* Set pinmux mode 7(GPIO) */
+    set_pinmux(PINCTRL_P8_03, 7);
+    set_pinmux(PINCTRL_P8_04, 7);
+    set_pinmux(PINCTRL_P8_05, 7);
+    set_pinmux(PINCTRL_P8_06, 7);
 
     return 0;
 }
